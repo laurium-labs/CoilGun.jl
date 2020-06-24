85253db4752dcae68e37092235cbe6bdaf801dbb
Eric <eric.halonen1@gmail.com>
2020-06-24 14:42:22 -0400

    full default sim

--- src/api/results.jl
+++ src/api/results.jl
@@ -6,11 +6,11 @@ function extract_results(sln)
     # println("Length of Velocity:\t\t",length(sln[4,:]),"\nLength of Position:\t\t", length(sln[3,:]),"\nLength of Time:\t\t\t", length(sln[2,:]),"\nLength of Magnetization:\t", length(sln[1,:]))
     # sln=solved_default(default)
     velocity = sln[3,:]
-    magentization= sln[1,:]
+    magnetization= sln[1,:]
     irreversibleMagentization=sln[4,:]
     # time=sln[0,:]
-    # displacement = sln[2,0]
+     displacement = sln[2,:]
 
     #body = Dict("time"=>stripped_time_core, "temperature" => stripped_temperature_core)
-    result_dict = Dict("velocity" => velocity, "magentization" => magentization, "irreversibleMagentization"=>irreversibleMagentization)
+    result_dict = Dict("velocity" => velocity, "magnetization" => magentization, "irreversibleMagentization"=>irreversibleMagentization,"displacement"=> displacement)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using CoilGun
+#using CoilGun
 using Unitful:Ω, m, cm, kg, g, A, N, Na, T, s, μ0, ϵ0, k, J, K, mol, me, q, ħ, μB, mm, inch, μm, H, V, gn
 using Unitful:Length, Mass, Current, Capacitance, Charge, Force, ElectricalResistance, BField, Volume, Area, Current, HField, MagneticDipoleMoment, Density, Inductance, ustrip, Voltage, Acceleration, Time, Velocity
 using ForwardDiff
