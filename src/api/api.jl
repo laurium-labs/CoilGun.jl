3e261c2396ccad55924d515e2b33b933975dfbd8
Eric <eric.halonen1@gmail.com>
2020-06-25 14:48:26 -0400

    undo

--- src/api/api.jl
+++ src/api/api.jl
@@ -18,8 +18,7 @@ function apply_json_mutations(object::Any, json::Dict)
 end
 function dictionary_api(dictionary_Coil_specification::Dict)::Dict
     scenario=apply_json_mutations(CoilGunDefaults.default_scenario, dictionary_Coil_specification)
-    return scenario
-    # extract_results(CoilGunDefaults.solve_senario(scenario))
+     extract_results(CoilGunDefaults.solve_senario(scenario))
     # x=rand(40)
     # return string(x)
 end
