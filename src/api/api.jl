c67d9b6546db99169d8db4d19a42ae86cfa47989
Eric <eric.halonen1@gmail.com>
2020-06-26 14:57:43 -0400

    format dict correct in test api and then for ui, should work

--- src/api/api.jl
+++ src/api/api.jl
@@ -1,24 +1,26 @@
 include("default_components.jl")
 include("results.jl")
-# function apply_json_mutations(object::Any, json::Dict)
-#     # This function recreates `object` with any mutations from `json`
-#      new_fields = map(fieldnames(typeof(object))) do field
-#         orig_field_value = getfield(object, field)
-#         if haskey(json, string(field))
-#             if orig_field_value isa Number
-#                 return typeof(orig_field_value)(json[string(field)])
-#             else
-#                 return apply_json_mutations(orig_field_value, json[string(field)])
-#             end
-#         else
-#             return orig_field_value
-#         end
-#     end
-#     return typeof(object)(new_fields...)
-# end
-function dictionary_api()#dictionary_Coil_specification::Dict)::Dict
-    # scenario=apply_json_mutations(CoilGunDefaults.default_scenario)
-     extract_results(CoilGunDefaults.solve_senario(CoilGunDefaults.default_scenario))
+function apply_json_mutations(object::Any, json::Dict)
+    # This function recreates `object` with any mutations from `json`
+     new_fields = map(fieldnames(typeof(object))) do field
+        orig_field_value = getfield(object, field)
+        if haskey(json, string(field))
+            if orig_field_value isa Number
+                return typeof(orig_field_value)(json[string(field)])
+            else
+                return apply_json_mutations(orig_field_value, json[string(field)])
+            end
+        else
+            return orig_field_value
+        end
+    end
+    return typeof(object)(new_fields...)
+end
+function dictionary_api(dictionary_Coil_specification::Dict)::Dict
+     scenario=apply_json_mutations(CoilGunDefaults.default_scenario, dictionary_Coil_specification)
+     println("1")
+     println("HHEEERRRREEEEE", scenario)
+     #extract_results(CoilGunDefaults.solve_senario(scenario))
     # x=rand(40)
     # return string(x)
 end
--- test/test_api.jl
+++ test/test_api.jl
@@ -1,2 +1,11 @@
 using CoilGun: dictionary_api
-println(dictionary_api())
+using Unitful:Ω, m, cm, kg, g, A, N, Na, T, s, μ0, ϵ0, k, J, K, mol, me, q, ħ, μB, mm, inch, μm, H, V, gn
+using Unitful:Length, Mass, Current, Capacitance, Charge, Force, ElectricalResistance, BField, Volume, Area, Current, HField, MagneticDipoleMoment, Density, Inductance, ustrip, Voltage, Acceleration, Time, Velocity
+barrelLength = "barrelLength"
+barrelThickness = "barellThickness"
+wireRadius = "wireRadius"
+coilLength = "coilLength"
+
+treeDict = Dict(barrelLength => "4", wireRadius => "4", coilLength => "4", barrelThickness => "4" )
+println(treeDict)
+dictionary_api(treeDict)
