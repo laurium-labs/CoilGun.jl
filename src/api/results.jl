eb0cc9d7af38d241fd097063ea1924f5b46a6b9c
Eric <eric.halonen1@gmail.com>
2020-07-06 10:08:44 -0400

    strip unit from endtime

--- src/api/results.jl
+++ src/api/results.jl
@@ -1,9 +1,10 @@
 using CoilGun: Scenario
+using Unitful: ustrip
 function extract_results(sln, scenario::Scenario)
     velocity = sln[3,:]
     magnetization= sln[1,:]
     irreversibleMagentization=sln[4,:]
     displacement = sln[2,:]
-    endTime = scenario.endTime
+    endTime = ustrip(scenario.endTime)
     result_dict = Dict( "velocity" => velocity, "magnetization" => magnetization, "irreversibleMagentization"=>irreversibleMagentization,"displacement"=> displacement,"endTime"=>endTime)
 end
